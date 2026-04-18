39fa03c523816e02e261162a44fa5b957d10f750
githubpractice12345 <anuruprocks@gmail.com>
2026-04-19 00:02:40 +0530

    dynamic tax module implementation

--- foodonline_main/static/js/custom.js
+++ foodonline_main/static/js/custom.js
@@ -231,9 +231,10 @@ $(document).ready(function(){
                     // subtotal, tax and grand_total by calling the fun
                     applyCartAmounts(
                         response.cart_amount['subtotal'],
-                        response.cart_amount['tax'],
+                        response.cart_amount['tax_dict'],
                         response.cart_amount['grand_total'],
                     )
+                    console.log(response.cart_amount['tax_dict'])
                 }else{
                     // Handle the failure message
                     Swal.fire({title: response.message, text: " ", icon: "error"})
@@ -286,7 +287,7 @@ $(document).ready(function(){
                     // subtotal, tax and grand_total by calling the fun
                     applyCartAmounts(
                         response.cart_amount['subtotal'],
-                        response.cart_amount['tax'],
+                        response.cart_amount['tax_dict'],
                         response.cart_amount['grand_total'],
                     )
                     
@@ -331,7 +332,7 @@ $(document).ready(function(){
                     // subtotal, tax and grand_total by calling the fun
                     applyCartAmounts(
                         response.cart_amount['subtotal'],
-                        response.cart_amount['tax'],
+                        response.cart_amount['tax_dict'],
                         response.cart_amount['grand_total'],
                     )
 
@@ -386,11 +387,22 @@ $(document).ready(function(){
     }
 
     //apply cart amounts
-    function applyCartAmounts(subtotal, tax, grand_total){
+    function applyCartAmounts(subtotal, tax_dict, grand_total){
         if(window.location.pathname == '/cart/'){
             $('#subtotal').html(subtotal)
-            $('#tax').html(tax)
+            // $('#tax').html(tax)
             $('#total').html(grand_total)
+
+            // console.log(tax_dict)
+            for(key1 in tax_dict){
+                // console.log(key1)
+                // console.log(tax_dict[key1])
+                for(key2 in tax_dict[key1]){
+                    // console.log(tax_dict[key1][key2])
+                    $('#tax-'+key1).html(tax_dict[key1][key2])
+                }
+
+            }
         }
         
     }
--- marketplace/models.py
+++ marketplace/models.py
@@ -19,3 +19,14 @@ class Cart(models.Model):
 
     # def __str__(self):
     #     return f"{self.user} - {self.fooditem} ({self.quantity})"
+
+class Tax(models.Model):
+    tax_type = models.CharField(max_length=20, unique=True)
+    tax_percentage = models.DecimalField(decimal_places=2, max_digits=4, verbose_name='Tax Percentage(%)')
+    is_active = models.BooleanField(default=True)
+
+    class Meta:
+        verbose_name_plural = 'tax'
+
+    def __str__(self):
+        return self.tax_type
