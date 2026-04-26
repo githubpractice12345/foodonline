c3c0f0921c0a47c9c19ebe18f195a1c44cf528ad
githubpractice12345 <anuruprocks@gmail.com>
2026-04-27 01:04:36 +0530

    razorpay payment gateway implementation

--- orders/views.py
+++ orders/views.py
@@ -8,6 +8,10 @@ import simplejson as json
 from .utils import generate_order_number
 from accounts.utils import send_notification
 from django.contrib.auth.decorators import login_required
+import razorpay
+from foodonline_main.settings import RZP_KEY_ID, RZP_KEY_SECRET
+
+client = razorpay.Client(auth=(RZP_KEY_ID, RZP_KEY_SECRET))
 
 # Create your views here.
 
@@ -45,9 +49,29 @@ def place_order(request):
             order.order_number = generate_order_number(order.id) #we are passing id that will be used as pk in number generation.
             order.save()
             # return redirect('place_order')
+
+            #RazorPay Payment
+            DATA = {
+                # "amount": float(order.total) * 100, 
+                "amount": int(order.total * 100), #RazorPay accepts only in paisa
+                "currency": "INR",
+                "receipt": "receipt#1"+order.order_number,
+                "notes": {
+                    "key1": "value3",
+                    "key2": "value2"
+                }
+            }
+            rzp_order = client.order.create(data=DATA)
+            # print(rzp_order)
+            rzp_order_id = rzp_order['id']
+
             context ={
                 'order': order,
                 'cart_items': cart_items,
+                'rzp_order_id': rzp_order_id,
+                'RZP_KEY_ID': RZP_KEY_ID,
+                # 'rzp_amount': float(order.total) * 100,
+                'rzp_amount': int(order.total * 100),
             }
             return render(request, 'orders/place_order.html', context)
         else:
